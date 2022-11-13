5d2d442c23162c7b73c3aeb4e4edc09a0adff40d
dragoncartoon <quocvuongtphcm@yahoo.com>
2022-11-13 16:43:14 +0700

    implement list page of Book, BookInstance, Genre, Author

--- app.js
+++ app.js
@@ -1,41 +1,51 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// create connection to mongoDB Atlas
+const mongoose = require("mongoose");
+const mongoDB =
+  "mongodb://m001-student:m001-mongodb-basics@ac-w4rdr9v-shard-00-00.5kplvwk.mongodb.net:27017,ac-w4rdr9v-shard-00-01.5kplvwk.mongodb.net:27017,ac-w4rdr9v-shard-00-02.5kplvwk.mongodb.net:27017/local_library?ssl=true&replicaSet=atlas-hvcrgy-shard-0&authSource=admin&retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.use(logger('dev'));
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
---
+++ controllers/authorController.js
@@ -0,0 +1,54 @@
+const { nextTick } = require("async");
+const Author = require("../models/author");
+
+// Display list of all Authors.
+exports.author_list = (req, res) => {
+  Author.find()
+    // .sort({family_name: 1}) Style goc
+    .sort([["family_name", "ascending"]])
+    .exec(function (err, list_author){
+      if (err) {
+        return next(err);
+      }
+      // Successful, so render
+      res.render("author_list", {
+        title: "Author list",
+        author_list: list_author,
+      });
+    });
+};
+
+// Display detail page for a specific Author.
+exports.author_detail = (req, res) => {
+  res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
+};
+
+// Display Author create form on GET.
+exports.author_create_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Author create GET");
+};
+
+// Handle Author create on POST.
+exports.author_create_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Author create POST");
+};
+
+// Display Author delete form on GET.
+exports.author_delete_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Author delete GET");
+};
+
+// Handle Author delete on POST.
+exports.author_delete_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Author delete POST");
+};
+
+// Display Author update form on GET.
+exports.author_update_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Author update GET");
+};
+
+// Handle Author update on POST.
+exports.author_update_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Author update POST");
+};
