385883cfc66d4e14897609f0b33ace90e15a258e
dragoncartoon <quocvuongtphcm@yahoo.com>
2022-11-18 15:37:54 +0700

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(helmet());
 
 // create connection to mongoDB Atlas
 const mongoose = require("mongoose");
-const mongoDB = process.env.DB_URL
-  
+const mongoDB = process.env.DB_URI
+
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
